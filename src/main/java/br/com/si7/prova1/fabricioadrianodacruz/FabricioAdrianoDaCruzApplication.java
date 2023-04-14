0af4b3a89c143c8e0c1a4e15f60406f9a79a639c
Fabricio Cruz <fabriciocruzz77@gmail.com>
2023-04-13 21:39:11 -0300

    Primeiro commit

---
+++ src/main/java/br/com/si7/prova1/fabricioadrianodacruz/FabricioAdrianoDaCruzApplication.java
@@ -0,0 +1,13 @@
+package br.com.si7.prova1.fabricioadrianodacruz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricioAdrianoDaCruzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricioAdrianoDaCruzApplication.class, args);
+	}
+
+}
